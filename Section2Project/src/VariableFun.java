31ae7c98235324c813048e66c54528a86826c5c6
Adam <adamlenter1@gmail.com>
2022-12-14 14:13:46 -0500

    Created a project for learning java. Two classes already created.

---
+++ Section2Project/src/VariableFun.java
@@ -0,0 +1,10 @@
+public class VariableFun {
+    public static void main(String[] args) {
+        int age = 19;
+        String name = "Billy";
+        String hometown = "Port Washington";
+
+        System.out.println(name + " is " + age + '\n');
+        System.out.println(hometown);
+    }
+}
