010ddba196a3e679f912dc2a901f80536fe95cb7
Adam <adamlenter1@gmail.com>
2022-12-14 14:29:17 -0500

    Created a file re: data type conversions.

---
+++ Section2Project/src/ConversionFun.java
@@ -0,0 +1,7 @@
+public class ConversionFun {
+    public static void main (String[] args) {
+        double myDouble = 3.14;
+        float myFloat = 3.14f; //By putting the f at the end, we are stating that it is a conversion.
+        double yourDouble = myFloat; //A double can hold a float, but a float can't hold a double.
+    }
+}
