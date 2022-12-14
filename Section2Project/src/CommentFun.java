5650d04c73ddac92b30a303db638280cf84efce2
Adam <adamlenter1@gmail.com>
2022-12-14 14:38:58 -0500

    Practice file for comments.

---
+++ Section2Project/src/CommentFun.java
@@ -0,0 +1,14 @@
+/*
+    Adam Lenter
+    Learning Comments in Javascript
+ */
+
+public class CommentFun {
+    /*
+    Main is the entry point to the application.
+     */
+    public static void main (String[] args) {
+        System.out.print("Printing Stuff");
+        //These are single line comments.
+    }
+}
--- Section2Project/src/ConversionFun.java
+++ Section2Project/src/ConversionFun.java
@@ -3,5 +3,14 @@ public class ConversionFun {
         double myDouble = 3.14;
         float myFloat = 3.14f; //By putting the f at the end, we are stating that it is a conversion.
         double yourDouble = myFloat; //A double can hold a float, but a float can't hold a double.
+
+        //byte
+        //int
+        //short
+        //long
+        //float
+        //double
+        //char
+        //boolean
     }
 }
